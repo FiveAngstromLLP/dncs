90e733e2b6e0c48fa42bcebc3f161c2bd370ba1e
Satheeshk369 <satheeshsk3693@gmail.com>
2024-10-13 07:01:06 +0530

    rm dist in leptos serve

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,21 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <div>
+            <h1>"Welcome to Leptos!"</h1>
+            <button
+                on:click=move |_| set_count.update(|count| *count += 1)
+            >
+                "Click me: " {count}
+            </button>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(App)
 }
