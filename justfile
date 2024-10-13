84e74a4e1dac48323ce9253e449b413017229132
Satheeshk369 <satheeshsk3693@gmail.com>
2024-10-13 05:41:01 +0530

    dncs single package

--- justfile
+++
@@ -1,8 +0,0 @@
-@run *ARGS:
-    cargo run -p dncs-cli {{ARGS}}
-
-@lib ARGS:
-    cd dncs-lib && cargo run --example {{ARGS}}
-
-@docs:
-    cd docs && mdbook serve
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-// use clap::Parser;
-use libdncs::parser::atoms_to_pdbstring;
-use libdncs::system::System;
+mod parser;
+mod system;
+
+use crate::parser::atoms_to_pdbstring;
+use crate::system::System;
 fn main() {
     let s = System::new("YGGFM");
     println!("{}", atoms_to_pdbstring(s.particles))
