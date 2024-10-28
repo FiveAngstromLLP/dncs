49cf8e5db37fb1abbf0743caaf8d8ea100d872f6
Satheeshk369 <satheeshsk3693@gmail.com>
2024-10-28 10:34:45 +0530

    minimize 5

--- src/minimizer.rs
+++ src/minimizer.rs
@@ -69,7 +69,7 @@ impl Minimizer {
 
             let mut theta: Vec<f64> = angle.clone();
             let prbs = lbfgs()
-                .with_max_iterations(10)
+                .with_max_iterations(5)
                 .minimize(&mut theta, evaluate, |_| false);
 
             match prbs {
