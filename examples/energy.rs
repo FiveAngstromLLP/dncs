475dbf031f491285e71f6005d6c905b4be41ec8c
Satheeshsk369 <satheeshsk3693@gmail.com>
2025-01-21 17:07:18 +0530

    energy

--- examples/energy.rs
+++ examples/energy.rs
@@ -7,10 +7,26 @@ const SEQUENCE: &str = "YGGFM";
 const FORCE_FIELD: FF = FF::AMBERFB15;
 
 fn main() {
+    let sys = System::new("BB", FORCE_FIELD.init());
+    for i in sys.particles.iter() {
+        println!("{:?}", i);
+    }
+
     // System
     let mut sys = System::new(SEQUENCE, FORCE_FIELD.init());
     sys.init_parameters();
     let amber = Amber::new(Arc::new(sys));
     let eng = amber.energy();
     println!("Energy: {} KCal/Mol", eng);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        let sys = System::new("BB", FORCE_FIELD.init());
+        println!("{}", sys.particles);
+    }
 }
